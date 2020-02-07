d5fedfa6704bed0cb260709a274f880c87c8f3d4
Chad Thomas <chad.c.thomas@gmail.com>
2020-02-07 07:11:43 -0500

    create instructor table row

---
+++ src/InstructorTableRow.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+
+export class InstructorTableRow extends Component {
+  render() {
+    let i = this.props.instructor;
+    return (
+      <tr>
+        <td>{i.id}</td>
+        <td>{i.name}</td>
+        <td>{i.title}</td>
+        <td>{i.courses.join(", ")}</td>
+        <td>
+          <button
+            className="btn btn-sm btn-warning m-1"
+            onClick={() => this.props.editCallback(i)}
+          >
+            Edit
+          </button>
+          <button
+            className="btn btn-sm btn-danger m-1"
+            onClick={() => this.props.deleteCallback(i)}
+          >
+            Delete
+          </button>
+        </td>
+      </tr>
+    );
+  }
+}
